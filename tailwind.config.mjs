48bbf3b7b1d5c6b643a5891ec5700dd3b79a4da4
Railly Hugo <railly_2009@hotmail.com>
2024-07-25 23:11:49 -0500

    feat: add landing sections - 80%

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        brick: ["NT Brick Sans", ...defaultTheme.fontFamily.sans],
+        space: ["Space Mono", ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        night: "#1F1F1F",
+        pineapple: "#FFDF6B",
+        sky: "#AAFCBE",
+        bone: "#FFFCEB",
+        strawberry: "#F6AFFB",
+        berries: "#FF704B",
+        grape: "#2F1B6D",
+        wine: "#5A2B6A",
+      },
+    },
+  },
+  plugins: [],
+};
